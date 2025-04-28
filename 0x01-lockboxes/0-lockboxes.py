cf3687409d62d5824833b6e016bcaf2a713f9826
FionaGachuuri <fionairuka@gmail.com>
2025-04-28 14:56:59 +0300

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,23 @@
-def canUnlockAll(boxes):
-    opened_boxes = set([0])  # Start with box 0 opened
-    keys = boxes[0][:]       # Copy keys from box 0
+#!/usr/bin/python3
+"""
+This module defines the canUnlockAll function,
+which determines if all boxes can be opened.
+The function takes a list of boxes, where each box contains a list of keys
+to other boxes.
+The function returns True if all boxes can be opened, otherwise False.
+The function uses a set to keep track of opened boxes and
+a list to manage keys.
+"""
 
+
+def canUnlockAll(boxes):
+    opened_boxes = set([0])
+    keys = boxes[0][:]
     while keys:
         key = keys.pop()
 
         if key < len(boxes) and key not in opened_boxes:
             opened_boxes.add(key)
-            keys.extend(boxes[key])  # Add new keys from this box
+            keys.extend(boxes[key])
 
     return len(opened_boxes) == len(boxes)
