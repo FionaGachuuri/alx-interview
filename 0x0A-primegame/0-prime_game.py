94c404bc3dbd31647fcc9f2a70669d61e4a8594d
FionaGachuuri <fionairuka@gmail.com>
2025-06-24 18:09:26 +0300

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,40 +8,39 @@ remaining after all possible moves.
 
 
 def isWinner(x, nums):
+    """Function to get who has won in prime game"""
     if x < 1 or not nums:
         return None
 
-    n = max(nums)
-    # Sieve of Eratosthenes to find primes up to n
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
+    mariaWinsCount = 0
+    benWinsCount = 0
 
-    for i in range(2, int(n**0.5) + 1):
+    # Precompute primes count up to the max number
+    max_num = max(nums)
+    is_prime = [True] * (max_num + 1)
+    is_prime[0:2] = [False, False]
+
+    for i in range(2, int(max_num ** 0.5) + 1):
         if is_prime[i]:
-            for j in range(i * i, n + 1, i):
+            for j in range(i * i, max_num + 1, i):
                 is_prime[j] = False
 
-    # Precompute the number of primes up to each number i
-    prime_count = [0] * (n + 1)
+    # Count primes up to each i
+    prime_counts = [0] * (max_num + 1)
     count = 0
-    for i in range(n + 1):
+    for i in range(len(prime_counts)):
         if is_prime[i]:
             count += 1
-        prime_count[i] = count
-
-    # Determine winner for each round
-    maria_wins = 0
-    ben_wins = 0
+        prime_counts[i] = count
 
     for num in nums:
-        if prime_count[num] % 2 == 1:
-            maria_wins += 1
+        if prime_counts[num] % 2 == 1:
+            mariaWinsCount += 1
         else:
-            ben_wins += 1
+            benWinsCount += 1
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+    if mariaWinsCount > benWinsCount:
+        return "Winner: Maria"
+    elif benWinsCount > mariaWinsCount:
+        return "Winner: Ben"
+    return None
